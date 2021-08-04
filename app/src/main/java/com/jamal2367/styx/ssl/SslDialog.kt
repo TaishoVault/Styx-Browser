6b696551697de473d67823fb2fbe18544c67fe94
Jamal <jamal2362@googlemail.com>
2021-08-04 21:04:18 +0200

    Improve SSL Dialog and add Algorithm OID.

--- app/src/main/java/com/jamal2367/styx/ssl/SslDialog.kt
+++ app/src/main/java/com/jamal2367/styx/ssl/SslDialog.kt
@@ -22,15 +22,17 @@ fun Context.showSslDialog(sslCertificate: SslCertificate, sslState: SslState) {
     val expireDate = sslCertificate.validNotAfterDate
     val dateFormat = DateFormat.getDateFormat(applicationContext)
     val contentView = inflater.inflate(R.layout.dialog_ssl_info, null, false).apply {
-        findViewById<TextView>(R.id.ssl_layout_issue_by).text = by.cName
-        findViewById<TextView>(R.id.ssl_layout_issue_to).text = to.oName?.takeIf(String::isNotBlank) ?: to.cName
+        findViewById<TextView>(R.id.ssl_layout_issue_by).text = by.dName
+        findViewById<TextView>(R.id.ssl_layout_issue_to).text = to.dName?.takeIf(String::isNotBlank) ?: to.cName
         findViewById<TextView>(R.id.ssl_layout_issue_date).text = dateFormat.format(issueDate)
         findViewById<TextView>(R.id.ssl_layout_expire_date).text = dateFormat.format(expireDate)
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
             val cert = sslCertificate.x509Certificate
             findViewById<TextView>(R.id.ssl_layout_serial_number).text = cert?.sigAlgName
+            findViewById<TextView>(R.id.ssl_layout_algorithm_oid).text = cert?.sigAlgOID
         } else {
             findViewById<TextView>(R.id.ssl_layout_serial_number).visibility = View.GONE
+            findViewById<TextView>(R.id.ssl_layout_algorithm_oid).visibility = View.GONE
             findViewById<TextView>(R.id.algorithm).visibility = View.GONE
         }
     }
